237e2abcbe1befc1a8e750adb0665c027516615b
ArthurBlasi <rthrblasi@gmail.com>
2024-08-06 11:47:43 -0300

    Aula 2 - módulos e funções

--- app.py
+++ app.py
@@ -1,4 +1,7 @@
-print("""
+import os
+
+def exibir_nome_do_programa():
+      print("""
     
 ░██████╗░█████╗░██████╗░░█████╗░██████╗░  ███████╗██╗░░██╗██████╗░██████╗░███████╗░██████╗░██████╗
 ██╔════╝██╔══██╗██╔══██╗██╔══██╗██╔══██╗  ██╔════╝╚██╗██╔╝██╔══██╗██╔══██╗██╔════╝██╔════╝██╔════╝
@@ -8,12 +11,48 @@ print("""
 ╚═════╝░╚═╝░░╚═╝╚═════╝░░╚════╝░╚═╝░░╚═╝  ╚══════╝╚═╝░░╚═╝╚═╝░░░░░╚═╝░░╚═╝╚══════╝╚═════╝░╚═════╝░
       """)
 
-print('1. Cadastrar restaurante')
-print('2. Listar restaurante')
-print('3. Ativar restaurante')
-print('4. Sair')
-
-opcao_escolhida = input('Escolha uma opção: ')
+def exibir_opcoes():
+    print('1. Cadastrar restaurante')
+    print('2. Listar restaurantes')
+    print('3. Ativar restaurante')
+    print('4. Sair')
 
 #print('Voce escolheu a opção', opcao_escolhida)
-print(f'Voce escolheu a opção {opcao_escolhida}')
+#print(f'Voce escolheu a opção {opcao_escolhida}')
+#print(opcao_escolhida == 1)
+#print(type(opcao_escolhida))
+#print(type(1))
+
+def finalizar_app():
+    os.system('cls')
+    print('Encerrando o programa\n')
+
+def escolher_opcao():
+    #opcao_escolhida = input('Escolha uma opção: ')
+    opcao_escolhida = int(input('Escolha uma opção: '))
+    #opcao_escolhida = int(opcao_escolhida)
+
+    #print('Voce escolheu a opção', opcao_escolhida)
+    #print(f'Voce escolheu a opção {opcao_escolhida}')
+    #print(opcao_escolhida == 1)
+    #print(type(opcao_escolhida))
+    #print(type(1))
+
+    if opcao_escolhida == 1:
+        print('Cadastrar restaurante')
+    elif opcao_escolhida == 2:
+        print('Listar restaurantes')
+    elif opcao_escolhida == 3:
+        print('Ativar restaurante')
+    else:
+        #print('Encerrando o programa')
+        finalizar_app()
+
+
+def main():
+      exibir_nome_do_programa()
+      exibir_opcoes()
+      escolher_opcao()
+
+if __name__ == '__main__':
+    main()
