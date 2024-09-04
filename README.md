e389802da818de56a56c83fd2a4c9e84996a2c56
ArthurBlasi <rthrblasi@gmail.com>
2024-09-04 15:21:15 -0300

    Aulas 2 e 3, construtor, propriedades e metodos de classe

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # Sabor-Express
-project developed during a python course
+This project was developed during a Python training course. As you can see, there are two packages, "func-app", which corresponds to course 1, where only functions were used, and "oo-app", which corresponds to course 2, where object orientation was used.
--- oo-app/modelos/restaurante.py
+++ oo-app/modelos/restaurante.py
@@ -1,14 +1,34 @@
 class Restaurante:
-    nome = ''
-    categoria = ''
-    ativo = False
 
-restaurante_praca = Restaurante()
-restaurante_praca.nome = 'Praça'
-restaurante_praca.categoria = 'Gourmet'
+    restaurantes = []
 
-restaurante_pizza = Restaurante()
+    def __init__(self, nome, categoria):
+        self._nome = nome.title()
+        self._categoria = categoria.upper()
+        self._ativo = False
+        Restaurante.restaurantes.append(self)
 
-restaurantes = [restaurante_praca, restaurante_pizza]
+    def __str__(self):
+        return f'{self._nome} | {self._categoria}'
 
-print(vars(restaurante_praca))
+    @classmethod
+    def listar_resturantes(cls):
+        print(f'{'Nome do restaurante'.ljust(20)} | {'Categoria'.ljust(20)} | {'Status'}')
+        for restaurante in cls.restaurantes:
+            print(f'{restaurante._nome.ljust(20)} | {restaurante._categoria.ljust(20)} | {restaurante.ativo}')
+
+    @property
+    def ativo(self):
+        return '✓' if self._ativo else 'X'
+
+    def alterar_estado(self):
+        self._ativo = not self._ativo
+
+restaurante_praca = Restaurante('Praça','Gourmet')
+#restaurante_praca.nome = 'Praça'
+#restaurante_praca.categoria = 'Gourmet'
+restaurante_pizza = Restaurante('Pizza','Italiana') 
+
+restaurante_praca.alterar_estado()
+
+Restaurante.listar_resturantes()
